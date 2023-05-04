c01638cdbbe552cc5d9d9192bbbe568d7f8793a7
01010100b <caveman1917@hotmail.com>
2023-05-04 22:38:35 +0200

    workaround for player-in-game not working on DE

---
+++ AgeScript.Compiler/Intrinsics/PlayerInGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeScript.Compiler.Intrinsics
+{
+    internal class PlayerInGame : Inlined
+    {
+        public override bool HasStringLiteral => false;
+
+        public PlayerInGame() : base()
+        {
+            ReturnType = Primitives.Bool;
+            Parameters.Add(new() { Name = "player_number", Type = Primitives.Int });
+        }
+
+        internal override void CompileCall(CompilationResult result, CallExpression cl, int? result_address = null, bool ref_result_address = false)
+        {
+            if (result_address is null)
+            {
+                return;
+            }
+
+            ExpressionCompiler.Compile(result, cl.Arguments[0], result.Memory.Intr0);
+            result.Rules.AddAction($"up-modify-sn 251 g:= {result.Memory.Intr0}");
+            result.Rules.AddAction($"up-modify-goal {result.Memory.Intr1} c:= 0");
+            result.Rules.StartNewRule($"player-in-game focus-player");
+            result.Rules.AddAction($"up-modify-goal {result.Memory.Intr1} c:= 1");
+            result.Rules.StartNewRule();
+            Utils.MemCopy(result, result.Memory.Intr1, result_address.Value, 1, false, ref_result_address);
+        }
+    }
+}
--- Quaternary/Program.cs
+++ Quaternary/Program.cs
@@ -6,23 +6,30 @@ namespace Quaternary
 {
     internal class Program
     {
+        private const string DE_FOLDER = @"F:\SteamLibrary\steamapps\common\AoE2DE\resources\_common\ai";
+        private const string WK_FOLDER = @"F:\AoE\WK\Voobly Mods\AOC\Data Mods\WololoKingdoms\Script.Ai";
+
         static void Main(string[] args)
         {
-            Compile();
-        }
-
-        private static void Compile()
-        {
-            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.json");
             var settings = new Settings()
             {
                 Name = "Quaternary",
                 SourceFolder = @"F:\Repos\01010100b\AgeScript\Quaternary\Source",
-                WorkingFolder = @"F:\AoE\WK\Voobly Mods\AOC\Data Mods\WololoKingdoms\Script.Ai",
-                DestinationFolder = @"F:\AoE\WK\Voobly Mods\AOC\Data Mods\WololoKingdoms\Script.Ai",
+                WorkingFolder = WK_FOLDER,
+                DestinationFolder = WK_FOLDER,
                 CompilerSettings = new()
             };
 
+            Compile(settings);
+
+            settings.WorkingFolder = DE_FOLDER;
+            settings.DestinationFolder = DE_FOLDER;
+            Compile(settings);
+        }
+
+        private static void Compile(Settings settings)
+        {
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.json");
             File.Delete(file);
             var options = new JsonSerializerOptions() { WriteIndented = true };
             File.WriteAllText(file, JsonSerializer.Serialize(settings, options));
