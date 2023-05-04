2fd459a48672305bc628635b7727192c55bb8e47
01010100b <caveman1917@hotmail.com>
2023-05-04 02:47:17 +0200

    improve on archer micro a bit

---
+++ AgeScript.Compiler/Intrinsics/Math/Lerp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeScript.Compiler.Intrinsics.Math
+{
+    internal class Lerp : Inlined
+    {
+        public override bool HasStringLiteral => false;
+
+        public Lerp() : base()
+        {
+            ReturnType = Primitives.Int2;
+            Parameters.Add(new() { Name = "point1", Type = Primitives.Int2 });
+            Parameters.Add(new() { Name = "point2", Type = Primitives.Int2 });
+            Parameters.Add(new() { Name = "amount", Type = Primitives.Int });
+        }
+
+        internal override void CompileCall(CompilationResult result, CallExpression cl, int? result_address = null, bool ref_result_address = false)
+        {
+            if (result_address is null)
+            {
+                return;
+            }
+
+            ExpressionCompiler.Compile(result, cl.Arguments[0], result.Memory.Intr0);
+            ExpressionCompiler.Compile(result, cl.Arguments[1], result.Memory.Intr2);
+            ExpressionCompiler.Compile(result, cl.Arguments[2], result.Memory.Intr4);
+            result.Rules.AddAction($"up-lerp-tiles {result.Memory.Intr0} {result.Memory.Intr2} g: {result.Memory.Intr4}");
+            Utils.MemCopy(result, result.Memory.Intr0, result_address.Value, 2, false, ref_result_address);
+        }
+    }
+}
---
+++ AgeScript.Compiler/Intrinsics/Players/PlayerStance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeScript.Compiler.Intrinsics.Players
+{
+    internal class PlayerStance : Inlined
+    {
+        public override bool HasStringLiteral => false;
+
+        public PlayerStance() : base()
+        {
+            ReturnType = Primitives.Bool;
+            Parameters.Add(new() { Name = "player", Type = Primitives.Int });
+            Parameters.Add(new() { Name = "stance", Type = Primitives.Int });
+        }
+
+        internal override void CompileCall(CompilationResult result, CallExpression cl, int? result_address = null, bool ref_result_address = false)
+        {
+            if (result_address is null)
+            {
+                return;
+            }
+
+            if (cl.Arguments[1] is not ConstExpression c1)
+            {
+                throw new Exception("stance must be const.");
+            }
+
+            ExpressionCompiler.Compile(result, cl.Arguments[0], result.Memory.Intr0);
+            result.Rules.AddAction($"up-modify-sn 251 g:= {result.Memory.Intr0}");
+            result.Rules.AddAction($"up-modify-goal {result.Memory.Intr1} c:= 0");
+
+            result.Rules.StartNewRule($"players-stance focus-player {c1.Int}");
+            result.Rules.AddAction($"up-modify-goal {result.Memory.Intr1} c:= 1");
+            result.Rules.StartNewRule();
+            Utils.MemCopy(result, result.Memory.Intr1, result_address.Value, 1, false, ref_result_address);
+        }
+    }
+}
