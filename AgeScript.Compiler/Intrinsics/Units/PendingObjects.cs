96e22ed67fb1ba3a365640826bce36d10fe2ed86
01010100b <caveman1917@hotmail.com>
2023-05-05 20:42:06 +0200

    use string literals for comparisons

---
+++ AgeScript.Compiler/Intrinsics/Units/PendingObjects.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeScript.Compiler.Intrinsics.Units
+{
+    internal class PendingObjects : Inlined
+    {
+        public override bool HasStringLiteral => true;
+
+        public PendingObjects() : base()
+        {
+            ReturnType = Primitives.Bool;
+            Parameters.Add(new() { Name = "object_id", Type = Primitives.Int });
+            Parameters.Add(new() { Name = "value", Type = Primitives.Int });
+        }
+
+        internal override void CompileCall(CompilationResult result, CallExpression cl, int? result_address = null, bool ref_result_address = false)
+        {
+            if (result_address is null)
+            {
+                return;
+            }
+
+            ExpressionCompiler.Compile(result, cl.Arguments[0], result.Memory.Intr0);
+            ExpressionCompiler.Compile(result, cl.Arguments[1], result.Memory.Intr1);
+            result.Rules.AddAction($"up-modify-goal {result.Memory.Intr2} c:= 0");
+            result.Rules.StartNewRule($"up-pending-objects g: {result.Memory.Intr0} g:{cl.Literal!.Replace("\"", string.Empty)} {result.Memory.Intr1}");
+            result.Rules.AddAction($"up-modify-goal {result.Memory.Intr2} c:= 1");
+            result.Rules.StartNewRule();
+            Utils.MemCopy(result, result.Memory.Intr2, result_address.Value, ReturnType.Size, false, ref_result_address);
+        }
+    }
+}
